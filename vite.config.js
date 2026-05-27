0fc6bfc3b581150ba936f70ee361649f4dde6bd9
samir <samiryousri092@gmail.com>
2026-05-27 16:07:42 +0300

    build to root

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite' // السطر ده مهم
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
-  plugins: [
-    react(),
-    tailwindcss(), // التفعيل هنا
-  ],
+  plugins: [react(), tailwindcss()],
   base: '/movies-app/',
+  build: {
+    outDir: '.', // <-- السطر ده هيخلي الملفات تطلع في الـ root بره علطول
+  }
 })
