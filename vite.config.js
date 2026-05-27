892395fd15d995f5ee9e94800615c8b6dfaad450
samir <samiryousri092@gmail.com>
2026-05-27 16:50:41 +0300

    fix paths for vercel production

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import tailwindcss from '@tailwindcss/vite' // السطر ده مهم
 
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  base: '/movies-app/', // سيب دي زي ما هي عشان لو احتجتها
+  plugins: [
+    react(),
+    tailwindcss(), // التفعيل هنا
+  ],
 })
